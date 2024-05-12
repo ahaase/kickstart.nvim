b90d81a2819dfd9fd9d75507fec42a53e87bb85e
Alex Haase <alexhaase@gmail.com>
2024-05-12 17:34:49 +0300

    format set

--- lua/alexh/set.lua
+++ lua/alexh/set.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = ' '
 
 vim.opt.mouse = ''
 
-vim.opt.guicursor = ""
+vim.opt.guicursor = ''
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -18,7 +18,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -28,7 +28,7 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = 'yes'
-vim.opt.isfname:append('@-@')
+vim.opt.isfname:append '@-@'
 
 vim.opt.updatetime = 50
 
