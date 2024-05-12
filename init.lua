d88468d690a9fdb251f028fda40b9eef3f091a1a
Alex Haase <alexhaase@gmail.com>
2024-05-12 18:08:59 +0300

    Familiar telscope commands

--- init.lua
+++ init.lua
@@ -286,15 +286,16 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = 'Telesco[P]e search [H]elp' })
+      -- vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telesco[P]e Search [F]iles' })
+      vim.keymap.set('n', '<leader>p.', builtin.builtin, { desc = 'Telesco[P]e search select' })
+      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git files' })
+      vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = 'Telesco[P]e search current [W]ord' })
+      vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telesco[P]e [S]earch by Grep' })
+      vim.keymap.set('n', '<leader>pd', builtin.diagnostics, { desc = 'Telesco[P]e search [D]iagnostics' })
+      vim.keymap.set('n', '<leader>prr', builtin.resume, { desc = 'Telesco[P]e search [RR]esume' })
+      vim.keymap.set('n', '<leader>prf', builtin.oldfiles, { desc = 'Telesco[P]e search [R]ecent [F]iles' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
