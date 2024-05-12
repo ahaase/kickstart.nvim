5b739320f3ebfe115236dc55a4750fe8c0608976
Alex Haase <alexhaase@gmail.com>
2024-05-12 15:02:32 +0300

    signature help remap

--- init.lua
+++ init.lua
@@ -507,6 +507,9 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Custom maps
+          map('<C-h', vim.lsp.buf.signature_help, '<ctrl>Signature [H]elp')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
