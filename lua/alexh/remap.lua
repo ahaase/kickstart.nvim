dc270947f3d48468499a91247052ac9e69954a97
Alex Haase <alexhaase@gmail.com>
2024-05-22 17:02:47 +0300

    oil plugin

--- lua/alexh/remap.lua
+++ lua/alexh/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Exit file' })
+-- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Exit file' })
 
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line(s) up' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line(s) down' })
